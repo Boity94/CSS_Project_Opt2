7a42b8f65fcf28fadfaf77d14bb84d6045cdf5cc
Boity94 <106741319+Boity94@users.noreply.github.com>
2024-02-06 02:22:23 +0200

    Update CO2_emission_app.py

--- CO2_emission_app.py
+++ CO2_emission_app.py
@@ -57,7 +57,7 @@ def visualize_data(df):
     if visualization_type == 'Line Chart':
         # Line chart example
         #fig, ax = plt.subplots(figsize=(10, 6))
-        sns.lineplot(x='Year', y='Emissions', data=df, ax=ax)
+        plt.lineplot(x='Year', y='Emissions', data=df, ax=ax)
         ax.set_title('CO2 Emission Over Time')
         ax.set_xlabel('Year')
         ax.set_ylabel('CO2 Emission')
@@ -67,14 +67,14 @@ def visualize_data(df):
         # Pie chart example
         pie_data = df['Emissions'].value_counts()
         #fig, ax = plt.subplots(figsize=(10, 6))
-        ax.pie(pie_data, labels=pie_data.index, autopct='%1.1f%%', startangle=90)
+        plt.pie(pie_data, labels=pie_data.index, autopct='%1.1f%%', startangle=90)
         ax.set_title('CO2 Emission Distribution by Country')
         st.write(fig)
     
     elif visualization_type == 'Histogram':
         # Histogram example
         #fig, ax = plt.subplots(figsize=(10, 6))
-        sns.histplot(df['Emissions'], bins=30, kde=True, ax=ax)
+        plt.hist(df['Emissions'], bins=30, kde=True, ax=ax)
         ax.set_title('Histogram of CO2 Emission')
         ax.set_xlabel('CO2 Emission')
         ax.set_ylabel('Frequency')
